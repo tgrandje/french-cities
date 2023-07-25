646816160952bfb2f94548ef8dc017b51d0c2bc0
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2023-07-25 11:27:10 +0200

    Update utils.py
    * speed up pynsee if credentials stored

--- french_cities/utils.py
+++ french_cities/utils.py
@@ -3,10 +3,14 @@
 from pynsee.utils import clear_all_cache
 from pynsee.utils.init_conn import init_conn
 import os
+from pathlib import Path
 
 
 def init_pynsee():
-    clear_all_cache()
-    keys = ["insee_key", "insee_secret", "http_proxy", "https_proxy"]
-    kwargs = {x: os.environ[x] for x in keys if x in os.environ}
-    init_conn(**kwargs)
+    home = str(Path.home())
+    pynsee_credentials_file = os.path.join(home, "pynsee_credentials.csv")
+    if not os.path.exists(pynsee_credentials_file):
+        clear_all_cache()
+        keys = ["insee_key", "insee_secret", "http_proxy", "https_proxy"]
+        kwargs = {x: os.environ[x] for x in keys if x in os.environ}
+        init_conn(**kwargs)
