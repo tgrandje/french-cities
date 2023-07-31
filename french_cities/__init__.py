3ebb43b9adac00c499e8f66412dce07a52d90df5
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2023-07-31 16:56:49 +0200

    self referencing of version

--- french_cities/__init__.py
+++ french_cities/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from dotenv import load_dotenv
+from importlib_metadata import version
 
 from french_cities.city_finder import find_city
 from french_cities.departement_finder import find_departements
@@ -8,7 +9,7 @@ from french_cities.vintage import set_vintage
 
 load_dotenv()
 
-__version__ = "0.1.0a11"
+__version__ = version(__package__)
 
 
 __all__ = [
