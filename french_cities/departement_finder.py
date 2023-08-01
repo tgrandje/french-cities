1c394d58acd8f2b686a51a99a65aadaf32197c80
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2023-08-01 17:30:26 +0200

    Fix KeyError on OpenDataSoft API's postreatment

--- french_cities/departement_finder.py
+++ french_cities/departement_finder.py
@@ -150,7 +150,7 @@ def _process_departements_from_postal(
             lambda xy: fuzz.token_set_ratio(*xy), axis=1
         )
 
-        results_cedex = results_cedex.sort_values(["codePostal", "score"])
+        results_cedex = results_cedex.sort_values([source, "score"])
         results_cedex = results_cedex.drop_duplicates(source, keep="last")
         results_cedex = results_cedex.drop(
             ["nom_com", "score", "libelle"], axis=1
