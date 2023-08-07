f5b45643fe6dc0649cefb8113eaac82e0c0141b4
thomas.grandjean <thomas.grandjean@developpement-durable.gouv.fr>
2023-08-07 11:47:38 +0200

    Use nominatim backend on tests

--- tests/test_city_finder.py
+++ tests/test_city_finder.py
@@ -144,7 +144,7 @@ input_df = pd.DataFrame(
     index=[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21],
 )
 
-output_df = find_city(input_df.copy(), epsg=4326)
+output_df = find_city(input_df.copy(), epsg=4326, use_nominatim_backend=True)
 
 
 class test_find_city(TestCase):
